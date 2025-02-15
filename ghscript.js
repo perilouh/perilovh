1288e07588441b8be102893ea3e1b7070373905d
peril <aepullen000@gmail.com>
2025-02-15 14:59:59 -0500

    skibidi webring

--- ghscript.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.github.com/repos/perilouh/perilovh/commits?per_page=1')
-.then(res => res.json())
-.then(res => {
-    let sha = res[0].sha;
-    let authorDate = new Date(res[0].commit.author.date);
-    document.getElementById('commitLatest').innerText = res[0].commit.message;
-    document.getElementById('shortHash').innerText = "latest commit:" + sha.substring(0, 7) + " on " + authorDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: '2-digit' });
-    document.getElementById('commitLink').href = "https://github.com/perilouh/perilovh/commit/" + sha
-});
